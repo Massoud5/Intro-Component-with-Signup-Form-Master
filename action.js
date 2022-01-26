94fcd225c423eac8c8eb3ec015b3d77f72a7ae30
Massoud5 <massoud.shams.2016@gmail.com>
2022-01-26 19:47:51 +0100

    updated

--- action.js
+++ action.js
@@ -1,25 +1,34 @@
 const form = document.querySelector("form")
 const inputs = document.querySelectorAll("input")
-let emailInput = document.getElementById("email")
-console.log(emailInput.value)
+const emailInput = document.getElementById("email")
+
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     inputs.forEach(input => {
         
         if(!input.value) {
             input.parentElement.classList.add("error")
-        } else {
-            input.parentElement.classList.remove("error")   
-            if (input.type == 'email'){
-                if (input.type == 'email'){
-                    if(validateEmail(input.value)) {
-                        input.parentElement.classList.remove("error")   
-                    } else {
-                        input.parentElement.classList.add("error")
-                      }
-                }           
+            input.classList.add("input-class")
+            if (input.name == "email"){
+                emailInput.placeholder="email@example/com"
+                emailInput.classList.add("email")
             }
-          }    
+        }   
+        else if (input.name == 'email'){ 
+
+            if(validateEmail(input.value)) {
+                    input.parentElement.classList.remove("error")   
+            } else {
+                input.parentElement.classList.add("error")
+                document.getElementById("email-error").style.display = "none"
+                document.getElementById("email-verify-error").style.display = "block"
+
+              }
+        } 
+        else {
+            input.parentElement.classList.remove("error")   
+        }        
+                
     }) 
 })
 
@@ -27,14 +36,3 @@ function validateEmail (email) {
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 }
-
-// document.getElementById("email").placeholder = "email@example/com"
-
-
-// if (input.type == 'email'){
-//     if(validateEmail(input.value)) {
-//         input.parentElement.classList.remove("error")   
-//     } else {
-//         input.parentElement.classList.add("error")
-//       }
-// }
